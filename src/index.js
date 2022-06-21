98533b4dcc4d78a8ea0bb5718085361ff96e5d80
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-21 16:55:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@
 // - Use the official-artwork object key as the images for the card. The images are all inside of the sprites key, in each pokemon object
 // pokemon.sprites.other['official-artwork'].front_default
 
+
+
 // - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
 
 console.log(data);
+
+function createPokemonCard(card) {
+    let liEl = document.createElement('li')
+    liEl.className = 'card'
+    let imgEl = document.createElement('img')
+    imgEl.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+    imgEl.alt = ''
+    imgEl.className = "card--img"
+    let h2El = document.createElement('h2')
+  h2El.textContent = `Name: ${'pokemon."pokemon"'}`
+  document.body.append(liEl)
+  liEl.append(imgEl)
+  h2El.innerText = 'Bulbasa'
+  
+     }
+
+     createPokemonCard(data) 
